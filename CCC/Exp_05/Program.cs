3ee79a6336c7aef9e54d65cd055ee01d58e669d2
simpo5000 <simpo5000@gmail.com>
2022-10-13 01:08:57 +0300

    Привет МАША

---
+++ CCC/Exp_05/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Привет чмырдяй,назовись: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же ЖОПА!!!!!!!!!!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+    
